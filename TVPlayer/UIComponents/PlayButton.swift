783c14237dd677f0d4d941e8ff155c610e6a13c3
Satin91 <cool19912015@gmail.com>
2024-01-05 20:40:15 +0300

    Animate play button with Core Animate

--- TVPlayer/UIComponents/PlayButton.swift
+++ TVPlayer/UIComponents/PlayButton.swift
@@ -10,22 +10,18 @@ import UIKit
 class PlayButton: UIView {
     
     enum Constants {
-        static let imagePadding: CGFloat = 14
-        static let tapAnimationDuration: CGFloat = 0.05
+        static let iconPadding: CGFloat = 14
+        static let tapAnimationDuration: CGFloat = 0.1
     }
     
     private var isPlay = false
-    private var playImage = Theme.Images.play
-    private var pauseImage = Theme.Images.pause
-    private let imageView = UIImageView()
-    
-    let blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
-    let shape = PlayAnimation()
+    private let blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
+    private let playButtonShape = PlayButtonShape()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
-        //        makeConstraints()
+        makeConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -39,49 +35,39 @@ class PlayButton: UIView {
     }
     
     public func setState(_ isPlay: Bool) {
-        shape.tap()
+        playButtonShape.animate()
         UIView.animate(withDuration: Constants.tapAnimationDuration) {
-            self.imageView.transform = CGAffineTransform(scaleX: 0.75, y: 0.75)
+            self.playButtonShape.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
         } completion: { _ in
-            self.imageView.image = isPlay ? self.pauseImage : self.playImage
-            UIView.animate(withDuration: Constants.tapAnimationDuration, delay: .zero, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8) {
-                self.imageView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            UIView.animate(withDuration: Constants.tapAnimationDuration) {
+                self.playButtonShape.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
             }
         }
     }
     
     private func setupView() {
-        playImage = playImage.withRenderingMode(.alwaysTemplate)
-        pauseImage = pauseImage.withRenderingMode(.alwaysTemplate)
-        addSubview(shape)
-        
-        //        addSubview(blurEffectView)
-        //        addSubview(imageView)
-        //        backgroundColor = .systemRed
-        //        tintColor = .white
-        backgroundColor = .mint
+        addSubview(blurEffectView)
+        addSubview(playButtonShape)
+        playButtonShape.animationDuration = Constants.tapAnimationDuration
     }
     
     private func tapped() {
-        UIView.animate(withDuration: 0.05) {
+        UIView.animate(withDuration: Constants.tapAnimationDuration / 2) {
             self.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
         } completion: { _ in
-            UIView.animate(withDuration: 0.2) {
+            UIView.animate(withDuration: Constants.tapAnimationDuration / 2) {
                 self.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
-                self.layer.opacity = 0
             }
         }
     }
     
     private func makeConstraints() {
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+        playButtonShape.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: topAnchor, constant: Constants.imagePadding),
-            imageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -Constants.imagePadding),
-            
-            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.imagePadding),
-            imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Constants.imagePadding)
+            playButtonShape.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.iconPadding),
+            playButtonShape.topAnchor.constraint(equalTo: topAnchor, constant: Constants.iconPadding),
+            playButtonShape.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Constants.iconPadding),
+            playButtonShape.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -Constants.iconPadding),
         ])
     }
     
@@ -90,27 +76,26 @@ class PlayButton: UIView {
         blurEffectView.frame = bounds
         layer.cornerRadius = bounds.width / 2
         layer.masksToBounds = true
-        shape.frame = CGRect(x: 12, y: 12, width: bounds.width - 24, height: bounds.height - 24)
-        
     }
 }
 
-final class PlayAnimation: UIView {
+fileprivate final class PlayButtonShape: UIView {
+    public var animationDuration: CGFloat = 0.3
+    public var initialState = false
     
-    private weak var displayLink: CADisplayLink?
     private var startTime: CFTimeInterval = 0
+    
     private var animateState = true
     
-    let animationDuration: CGFloat = 1
-    
-    private let radius: CGFloat = 5
-    private let rectWidth: CGFloat = 12
-    private let rectSpacing: CGFloat = 20
-    
+    private let radius: CGFloat = 4
+    private let rectWidth: CGFloat = 10
+    private var spacing: CGFloat {
+        rectWidth
+    }
     private let rightFigure: CAShapeLayer = {
         let shapeLayer = CAShapeLayer()
         shapeLayer.strokeColor = UIColor.white.cgColor
-        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.fillColor = UIColor.white.cgColor
         shapeLayer.lineWidth = 2
         return shapeLayer
     }()
@@ -118,116 +103,65 @@ final class PlayAnimation: UIView {
     private let leftFigure: CAShapeLayer = {
         let shapeLayer = CAShapeLayer()
         shapeLayer.strokeColor = UIColor.white.cgColor
-        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.fillColor = UIColor.white.cgColor
         shapeLayer.lineWidth = 2
         return shapeLayer
     }()
     
-    var leftRect: CGMutablePath {
-        drawRect(leftX: .zero, rightX: rectWidth, radius: radius)
+    private var leftRect: CGMutablePath {
+        drawRect(leftX: bounds.midX - spacing / 2 - rectWidth, rightX: bounds.midX - spacing / 2, radius: radius, topPadding: spacing / 2)
     }
     
-    var rightRect: CGMutablePath {
-        drawRect(leftX: rectWidth + rectSpacing, rightX: rectWidth + rectSpacing + rectWidth, radius: radius)
+    private var rightRect: CGMutablePath {
+        drawRect(leftX: bounds.midX + spacing / 2, rightX: bounds.midX + spacing / 2 + rectWidth, radius: radius, topPadding: spacing / 2)
     }
     
-    var triangle: CGMutablePath {
+    private var triangle: CGMutablePath {
         return drawTriangle()
     }
     
-    public func tap() {
+    private var zeroPath: CGMutablePath {
+        drawRect(leftX: spacing, rightX: spacing + 0.5, radius: 0.25, topPadding: spacing + radius + 2)
+    }
+    
+    public func animate() {
         startTime = CACurrentMediaTime()
         animateState.toggle()
-        
+        print("Animate state \(animateState)")
         let rightReverseAnimation = CABasicAnimation(keyPath: "path")
         rightReverseAnimation.duration = animationDuration
         rightReverseAnimation.fromValue = animateState == false ? rightRect : triangle
         rightReverseAnimation.toValue = animateState == false ? triangle : rightRect
         rightReverseAnimation.timingFunction = CAMediaTimingFunction(name: .default)
-        rightReverseAnimation.isRemovedOnCompletion = false
-        rightReverseAnimation.fillMode = .backwards
-        rightFigure.removeAnimation(forKey: "animateCard")
-        rightFigure.add(rightReverseAnimation, forKey: "animateCard")
-//        CATransaction.commit()
+        rightFigure.removeAnimation(forKey: "animateTriangle")
+        rightFigure.add(rightReverseAnimation, forKey: "animateTriangle")
         
         let leftReverseAnimation = CABasicAnimation(keyPath: "path")
         leftReverseAnimation.duration = animationDuration
-        leftReverseAnimation.fromValue = animateState == false ? leftRect : leftZeroPath()
-        leftReverseAnimation.toValue = animateState == false ? leftZeroPath() : leftRect
+        leftReverseAnimation.fromValue = animateState == false ? leftRect : zeroPath
+        leftReverseAnimation.toValue = animateState == false ? zeroPath : leftRect
         leftReverseAnimation.timingFunction = CAMediaTimingFunction(name: .default)
-        leftReverseAnimation.isRemovedOnCompletion = false
-        leftReverseAnimation.fillMode = .backwards
-        leftFigure.removeAnimation(forKey: "animateCard2")
-        leftFigure.add(leftReverseAnimation, forKey: "animateCard2")
-//        CATransaction.commit()
+        leftFigure.removeAnimation(forKey: "animateRect")
+        leftFigure.add(leftReverseAnimation, forKey: "animateRect")
         
         if animateState {
-            rightFigure.path = rightRectPath()
+            rightFigure.path = rightRect
             leftFigure.path = leftRect
         } else {
-            rightFigure.path = drawTriangle()
-            leftFigure.path = leftZeroPath()
+            rightFigure.path = triangle
+            leftFigure.path = zeroPath
         }
-    }
-    
-    private func leftZeroPath() -> CGMutablePath {
-        drawRect(leftX: .zero, rightX: 0.5, radius: 0)
-    }
-    
-    private func leftRectPath() -> CGMutablePath {
-        let leftX: CGFloat = 0
-        let rightX = rectWidth
-        let path = CGMutablePath()
-        
-        let middleLeft = CGPoint(x: leftX, y: bounds.midY)
-        let topLeft = CGPoint(x: leftX, y: .zero)
-        let topRight = CGPoint(x: rightX, y: .zero)
-        let bottomLeft = CGPoint(x: leftX, y: bounds.height)
-        let bottomRight = CGPoint(x: rightX, y: bounds.height)
-        
-        path.move(to: middleLeft)
-        
-        path.addArc(tangent1End: topLeft, tangent2End: topRight, radius: radius)
-        path.addArc(tangent1End: topRight, tangent2End: bottomRight, radius: radius)
-        path.addArc(tangent1End: bottomRight, tangent2End: bottomLeft, radius: radius)
-        path.addArc(tangent1End: bottomLeft, tangent2End: topLeft, radius: radius)
-        path.closeSubpath()
-        return path
-    }
-    
-    private func rightRectPath() -> CGMutablePath {
-        let leftX = rectWidth + rectSpacing
-        let rightX = rectSpacing + rectWidth + rectWidth
-        let path = CGMutablePath()
-        
-        let middleLeft = CGPoint(x: leftX, y: bounds.midY)
-        let topLeft = CGPoint(x: leftX, y: .zero)
-        let topRight = CGPoint(x: rightX, y: .zero)
-        let bottomLeft = CGPoint(x: leftX, y: bounds.height)
-        let bottomRight = CGPoint(x: rightX, y: bounds.height)
-        
-        path.move(to: middleLeft)
-        
-        path.addArc(tangent1End: topLeft, tangent2End: topRight, radius: radius)
-        path.addArc(tangent1End: topRight, tangent2End: bottomRight, radius: radius)
-        path.addArc(tangent1End: bottomRight, tangent2End: bottomLeft, radius: radius)
-        path.addArc(tangent1End: bottomLeft, tangent2End: topLeft, radius: radius)
-        path.closeSubpath()
-        
-        return path
     }
     
     private func drawTriangle() -> CGMutablePath {
         let path = CGMutablePath()
-        let height = bounds.height
+        let padding: CGFloat = 6
         
-        // The triangle's three corners.
-        let topLeft = CGPoint(x: 0, y: 0)
-        let middleLeft = CGPoint(x: .zero, y: bounds.midY)
+        let topLeft = CGPoint(x: padding, y: .zero)
+        let middleLeft = CGPoint(x: padding, y: bounds.midY)
         let middleRight = CGPoint(x: bounds.maxX, y: bounds.midX)
-        let bottomLeft = CGPoint(x: 0, y: height)
+        let bottomLeft = CGPoint(x: padding, y: bounds.height)
         
-        // Draw three arcs to trace the triangle.
         path.move(to: middleLeft)
         path.addArc(tangent1End: topLeft, tangent2End: middleRight, radius: radius)
         path.addArc(tangent1End: middleRight, tangent2End: bottomLeft, radius: radius)
@@ -236,17 +170,16 @@ final class PlayAnimation: UIView {
         return path
     }
     
-    private func drawRect(leftX: CGFloat, rightX: CGFloat, radius: CGFloat) -> CGMutablePath {
+    private func drawRect(leftX: CGFloat, rightX: CGFloat, radius: CGFloat, topPadding: CGFloat = 0) -> CGMutablePath {
         let path = CGMutablePath()
         
         let middleLeft = CGPoint(x: leftX, y: bounds.midY)
-        let topLeft = CGPoint(x: leftX, y: .zero)
-        let topRight = CGPoint(x: rightX, y: .zero)
-        let bottomLeft = CGPoint(x: leftX, y: bounds.height)
-        let bottomRight = CGPoint(x: rightX, y: bounds.height)
+        let topLeft = CGPoint(x: leftX, y: topPadding)
+        let topRight = CGPoint(x: rightX, y: topPadding)
+        let bottomLeft = CGPoint(x: leftX, y: bounds.height - topPadding)
+        let bottomRight = CGPoint(x: rightX, y: bounds.height - topPadding)
         
         path.move(to: middleLeft)
-        
         path.addArc(tangent1End: topLeft, tangent2End: topRight, radius: radius)
         path.addArc(tangent1End: topRight, tangent2End: bottomRight, radius: radius)
         path.addArc(tangent1End: bottomRight, tangent2End: bottomLeft, radius: radius)
@@ -257,15 +190,17 @@ final class PlayAnimation: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        layer.addSublayer(rightFigure)
-        layer.addSublayer(leftFigure)
+        setupView()
     }
-    
-    deinit {
-        displayLink?.invalidate()
-    }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        layer.addSublayer(leftFigure)
+        layer.addSublayer(rightFigure)
+        leftFigure.path = leftRect
+        rightFigure.path = rightRect
     }
 }
